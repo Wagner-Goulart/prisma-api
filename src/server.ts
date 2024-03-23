28e5dad7f72c1c35ee7de4b18232216d1492a33e
Wagner <wagnergoulart0@gmail.com>
2024-03-23 19:03:24 -0300

    HOTFIX: Cors
    Signed-off-by: Wagner <wagnergoulart0@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors"
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(router)
 
@@ -14,3 +16,4 @@ process.on("SIGINT", ()=>{
   server.close()
   console.log("Servidor finalizado")
 })
+
